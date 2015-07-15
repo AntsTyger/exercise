17273ae6c02fc988206fc9d403236e39771e40c3
Sian Nadin <nadin.sian@gmail.com>
2015-07-15 16:02:55 +0000

    graph linked to db but not displaying

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,27 @@
 class Workout < ActiveRecord::Base
-	has_many :exercises, dependent: :destroy
+ 
+  has_many :exercises, dependent: :destroy
+
+  def self.chart_data(start = 1.weeks.ago)
+    total_lengths = lengths_by_day(start)
+    strength_lengths = where(strength: true).lengths_by_day(start)
+    cardio_lengths = where(strength: false).lengths_by_day(start)
+    (start.to_date..Date.today).map do |date|
+      {
+        date: date,
+        length: total_lengths[date] || 0,
+        strength_length: strength_lengths[date] || 0,
+        cardio_length: cardio_lengths[date] || 0
+      }
+    end
+  end
+
+  def self.lengths_by_day(start)
+    workouts = where(date: start.beginning_of_day..Time.zone.now)
+    workouts = workouts.group("date(date)")
+    workouts = workouts.select("date, sum(length) as total_length")
+    workouts.each_with_object({}) do |workout, lengths|
+      lengths[workout.date.to_date] = workout.total_length
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722223412) do
+ActiveRecord::Schema.define(version: 20150722223413) do
 
   create_table "directions", force: true do |t|
     t.text     "step"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20150722223412) do
     t.integer  "length"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "strength"
   end
 
 end
