8b9b46c31cbfceceeaa74f47b83ab4e5eabdbcec
Sian Nadin <nadin.sian@gmail.com>
2015-07-26 10:31:27 +0000

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725172155) do
+ActiveRecord::Schema.define(version: 20150726102901) do
 
   create_table "directions", force: true do |t|
     t.text     "step"
@@ -59,10 +59,18 @@ ActiveRecord::Schema.define(version: 20150725172155) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  add_index "routines", ["user_id"], name: "index_routines_on_user_id"
+
+  create_table "stats", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
@@ -84,8 +92,7 @@ ActiveRecord::Schema.define(version: 20150725172155) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
